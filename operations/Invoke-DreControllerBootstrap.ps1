dce9ec5a7a6618b6f675d12a512c4c40e01c06b2
Gleison Sette <gleisonsette@gmail.com>
2026-09-02 07:07:30 -0300

    chore(dre): pin first deploy recovery controller

--- operations/Invoke-DreControllerBootstrap.ps1
+++ operations/Invoke-DreControllerBootstrap.ps1
@@ -8,9 +8,9 @@ Import-Module (Join-Path $PSScriptRoot 'Dre.SudoBootstrap.psm1') -Force
 
 $server = 'apiadmin@192.168.100.59'
 $remoteRoot =
-    '/home/apiadmin/dre-controller-bootstrap-4902604dad96-20260902T075810Z'
+    '/home/apiadmin/dre-controller-bootstrap-4902604dad96-20260902T100430Z'
 $archiveSha256 =
-    'af943097715fb73f32d1aecba8aa6bc28f2b19bf414841357f0a6369c1f30c47'
+    'c544f8c5eb73ceaed284fe2d7b0dc9944ba9ffc5b81d5f80084eb6fe81d04b90'
 $publicKeySha256 =
     '4902604dad96d9b07f4010308d30e3815cb4e76446855d925079be0e3b922ce9'
 $sshDirectory = Join-Path $env:LOCALAPPDATA 'apiwpp\ssh'
